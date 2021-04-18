80ec8dfd290588877c3a008faaaffbe32bd6cf0e
kurtjacob <kurtjacob.urquico@gmail.comb>
2021-04-18 15:13:20 +0800

    add TypesAndVariables.java

---
+++ Java/Lab_Exercise_01/TypesAndVariables.java
@@ -0,0 +1,8 @@
+public class TypesAndVariables {
+    public static void main(String[] args) {
+        double amount;
+        amount = 5.95;
+        amount = amount + 25.00;
+        System.out.println(amount);
+    }
+}
