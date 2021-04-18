bd223704ecdf34c650b53f7eac8e5c5212477fbf
kurtjacob <kurtjacob.urquico@gmail.comb>
2021-04-18 15:09:41 +0800

    add VariablesSample.java

---
+++ Java/Lab_Exercise_01/VariableSample.java
@@ -0,0 +1,15 @@
+public class VariableSample {
+    public static void main(String[] args) {
+        // declare a data type with variable name and result and boolean data type
+        boolean result = true;
+        // declare a data type with variable name option and char data type
+        char option;
+        option = 'C'; // assign 'C' to option
+        // declare a data type with variable name grade, double data type and initialized to 0.0
+        double grade = 0.0;
+
+        System.out.println(result);
+        System.out.println(option);
+        System.out.println(grade);
+    }
+}
