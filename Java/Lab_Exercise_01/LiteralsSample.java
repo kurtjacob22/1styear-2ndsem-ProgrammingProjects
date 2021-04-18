99c29481b47049d2e8d875560520f1b777b0c203
kurtjacob <kurtjacob.urquico@gmail.comb>
2021-04-18 15:02:50 +0800

    add LiteralsSample.java

--- Java/Lab_Exercise_01/LiteralsSample.java
+++ Java/Lab_Exercise_01/LiteralsSample.java
@@ -1,3 +1,17 @@
 public class LiteralsSample {
-    
+    public static void main(String[] args) {
+        int dec = 12;
+        int hex = 0xc;
+        int oct = 014;
+        float Fex = 583.45f;
+        double Dex = 583.45;
+        float Sn = 5.8345e2f;
+
+        System.out.println(dec);
+        System.out.println(hex);
+        System.out.println(oct);
+        System.out.println(Fex);
+        System.out.println(Dex);
+        System.out.println(Sn);
+    }
 }
