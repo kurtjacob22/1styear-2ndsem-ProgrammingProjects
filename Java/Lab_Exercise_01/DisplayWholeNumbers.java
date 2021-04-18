c6993e04ac35ed58fb491aef1ea0ccf3a98030c6
kurtjacob <kurtjacob.urquico@gmail.comb>
2021-04-18 15:16:57 +0800

    add DisplayWholeNumbers.java

---
+++ Java/Lab_Exercise_01/DisplayWholeNumbers.java
@@ -0,0 +1,15 @@
+public class DisplayWholeNumbers {
+    public static void main(String[] args) {
+        int examScore = 87;
+        double averageGrade = 84.67;
+        byte seconds_per_minute = 60;
+        double length = 29.7;
+        long referenceNumber = 998883723193101L;
+
+        System.out.println((double)examScore);
+        System.out.println((int)averageGrade);
+        System.out.println(seconds_per_minute);
+        System.out.println((int)length);
+        System.out.println(referenceNumber);
+    }
+}
