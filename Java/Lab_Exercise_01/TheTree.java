94664b8b5459644c358d8f047e81b35db5da9bad
kurtjacob <kurtjacob.urquico@gmail.comb>
2021-04-18 14:57:33 +0800

    changes in salary_commision folder

---
+++ Java/Lab_Exercise_01/TheTree.java
@@ -0,0 +1,9 @@
+
+public class TheTree {
+    public static void main(String[] args) {
+        System.out.println("I think that i shall never see,");
+        System.out.println("a poem as lovely as a tree");
+        System.out.println("A tree whose hungry mouth is pressed");
+        System.out.println("Against the Earth's sweet flowing breast");
+    }
+}
--- Java/Sales_Commision/Salary.java
+++ Java/Sales_Commision/Salary.java
@@ -1,4 +1,4 @@
-package Java.Sales_Commision;
+
 
 import java.util.Scanner;
 
